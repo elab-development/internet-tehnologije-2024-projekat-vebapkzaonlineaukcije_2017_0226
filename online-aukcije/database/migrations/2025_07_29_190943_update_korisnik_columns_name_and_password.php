62934826daa11f07656152670ec0646f04a598ef
Pavle Vojinovic <pavletenis@gmail.com>
2025-07-29 21:48:33 +0200

    Dodavanje novih, promena naziva postojecih i brisanje kolona u tabeli Korisnik

---
+++ online-aukcije/database/migrations/2025_07_29_190943_update_korisnik_columns_name_and_password.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('korisnik', function (Blueprint $table) {
+            $table->renameColumn('name', 'ime');
+            $table->renameColumn('password', 'lozinka');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('korisnik', function (Blueprint $table) {
+            $table->renameColumn('ime', 'name');
+            $table->renameColumn('lozinka', 'password');
+        });
+    }
+};
