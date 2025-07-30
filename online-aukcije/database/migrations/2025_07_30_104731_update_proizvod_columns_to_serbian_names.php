bf70808c8aecfe507c5f1bc0f3e565d449aa2372
Pavle Vojinovic <pavletenis@gmail.com>
2025-07-30 13:35:24 +0200

    Promena naziva kolona u tabeli Proizvod

---
+++ online-aukcije/database/migrations/2025_07_30_104731_update_proizvod_columns_to_serbian_names.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('proizvod', function (Blueprint $table) {
+            $table->renameColumn('name', 'naziv');
+            $table->renameColumn('description', 'opis');
+            $table->renameColumn('category', 'kategorija');
+            $table->renameColumn('state', 'stanje');
+            $table->renameColumn('pictureURL', 'slikaURL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('proizvod', function (Blueprint $table) {
+            $table->renameColumn('naziv', 'name');
+            $table->renameColumn('opis', 'description');
+            $table->renameColumn('kategorija', 'category');
+            $table->renameColumn('stanje', 'state');
+            $table->renameColumn('slikaURL', 'pictureURL');
+        });
+    }
+};
