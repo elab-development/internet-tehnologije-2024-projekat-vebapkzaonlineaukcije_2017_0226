e29a3f5e76e25e360666b979c97034bef0c8ad22
Pavle Vojinovic <pavletenis@gmail.com>
2025-08-01 23:02:33 +0200

    Ponovo napravljena sessions tabela i ispravljena greska kod migracija

--- online-aukcije/database/migrations/2025_07_31_112904_drop_unwanted_initial_tables.php
+++ online-aukcije/database/migrations/2025_07_31_112904_drop_unwanted_initial_tables.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::dropIfExists('jobs');
         Schema::dropIfExists('job_batches');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        //Schema::dropIfExists('sessions');
         
         
     }
@@ -33,14 +33,14 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+        //Schema::create('sessions', function (Blueprint $table) {
+           // $table->string('id')->primary();
+           // $table->foreignId('user_id')->nullable()->index();
+           // $table->string('ip_address', 45)->nullable();
+           // $table->text('user_agent')->nullable();
+           // $table->longText('payload');
+           // $table->integer('last_activity')->index();
+        //});
 
        // Schema::create('cache', function (Blueprint $table) {
         //    $table->string('key')->primary();
